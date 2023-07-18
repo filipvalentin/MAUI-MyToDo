caa21b988babc787e43c8e96b04e69efdc11fc6f
theDotAfterExe <valentin.filip@student.uaic.ro>
2023-07-18 22:37:53 +0300

    Add project files.

---
+++ Entities/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace MyToDo.Entities {
+	class ToDoItem {
+
+		public Guid Id { get; set; }
+		public String Title { get; set; }
+		public DateTime Deadline { get; set; }
+		public bool IsRecurring { get; set; }
+
+	}
+}
