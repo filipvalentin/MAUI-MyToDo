9b960de65469c705b01b4a983634463818846ab2
Filip Valentin <100225320+filipvalentin@users.noreply.github.com>
2023-07-23 17:06:42 +0300

    Add files via upload

---
+++ BackgroundWorker/Entities/ToDoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MyToDo.Entities {
+	public class ToDoItem {
+
+		public Guid Id { get; set; }
+		public String Title { get; set; }
+		public DateTime Deadline { get; set; }
+		//public bool IsRecurring { get; set; }
+		public bool Acknowledged { get; set; }
+
+	}
+}
